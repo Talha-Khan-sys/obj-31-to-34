4f9a6f0550e73a76409e4f17711f8d2bda97ffcb
Muhammad Talha Khan <hdmoviez1234@gmail.com>
2021-11-26 23:45:25 +0500

    obj 31 to 34

---
+++ app.js
@@ -0,0 +1,55 @@
+// var a =new Date();
+// console.log(a);
+
+
+// var today = new Date();
+// var month = today.getMonth(); 
+// var monthlist =["January","February","March","April","May","June","July","August","September","October","November","December"]
+// console.log("Current Month" + ": " + monthlist[month])
+
+
+// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(0,3);
+// console.log(c)
+
+
+// var today = new Date();
+// var day = today.getDay();
+// var daylist = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+// if(day == 5){
+//     alert("Oops It's Weekday!")
+// }else{
+//     alert("It's FUN day!")
+// }
+
+
+// var today = new Date();
+// var date = today.getDate();
+// if(date === 16){
+//     alert("First fifteen days of the month")
+// }else{
+//     alert("Last days of the month")
+// }
+
+
+// var today = new Date();
+// var milisec = today.getTime();
+// console.log("Current Date" + ": " + today)
+// console.log("Elapsed Miliseconds Since Janaury 1 ,1970" + ": " + milisec)
+
+
+// var dob = new Date( prompt("Enter Your Date Of Birth"));
+// var dobmili = dob.getTime();
+// var toddate = new Date();
+// var todmili = toddate.getTime();
+// var diff = todmili - dobmili;
+// var accuage = Math.floor(diff/(1000*60*60*24*30*12))
+// console.log("Your age is" + ": " + accuage);
+// console.log("Your Date of Birth" + ": " + dob)
+
+
+// var today = new Date();
+// var back100 = today.setUTCFullYear(1921);
+// console.log("Current Date" + ": " + today);
+// console.log("100 Year Back" + ": " + back100)
